e239b197d4b178684de288b89fce7e1a5a1c4fc0
Tobias Weyand <weyand@google.com>
2020-07-11 12:12:17 -0700

    Apply Apache 2.0 license to NitroTracker Tobkit

--- tobkit/source/piano.pal.c
+++ tobkit/source/piano.pal.c
@@ -1,3 +1,19 @@
+/*====================================================================
+Copyright 2006 Tobias Weyand
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+======================================================================*/
+
 const unsigned short piano_Palette[16] = {
 0x0000, 0x0421, 0x0842, 0x0c63, 0x1084, 0x14a5, 0x18c6, 0x1ce7, 
 0x2108, 0x6318, 0x6b5a, 0x6f7b, 0x739c, 0x77bd, 0x7bde, 0x7c1f
--- tobkit/source/piano_hit.c
+++ tobkit/source/piano_hit.c
@@ -1,3 +1,19 @@
+/*====================================================================
+Copyright 2006 Tobias Weyand
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+======================================================================*/
+
 #include <nds.h>
 const u8 piano_hit[5][28] = {
 {0, 1, 1, 3, 3, 4, 5, 6, 6, 8, 8, 10, 10, 11, 12, 13, 13, 15, 15, 16, 17, 18, 18, 20, 20, 22, 22, 23},
